cab0f3756d2abb21c8b66548bf76ce5c842e846b
NeRaG0n7 <robotvali@gmail.com>
2023-06-13 15:51:21 +0300

    Added tailwind css

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    purge: [],
+    darkMode: true, // or 'media' or 'class'
+    theme: {
+      extend: {},
+    },
+    variants: {
+      extend: {},
+    },
+    plugins: [],
+  };
+  
