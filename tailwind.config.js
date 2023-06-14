181f378bc5d92bfcbdbfc31ccb4c590b335c4b14
NeRaG0n7 <robotvali@gmail.com>
2023-06-14 04:37:33 +0300

    update v1.1

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,8 @@
+/** @type {import('tailwindcss').Config} */
 module.exports = {
-    purge: [],
-    darkMode: true, // or 'media' or 'class'
-    theme: {
-      extend: {},
-    },
-    variants: {
-      extend: {},
-    },
-    plugins: [],
-  };
-  
+  content: ["./*.{html,js}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
